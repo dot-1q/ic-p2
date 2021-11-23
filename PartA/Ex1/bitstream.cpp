09bbd3091c94629a7db86f24de079d3ccf451a34
tiago <tiagobarros99@ua.pt>
2021-11-23 23:38:43 +0000

    read and write 1 bit done

--- PartA/Ex1/bitstream.cpp
+++ PartA/Ex1/bitstream.cpp
@@ -1,4 +1,6 @@
+#include <cstdio>
 #include <iostream>
+#include <limits>
 #include <fstream>
 
 using namespace std;
@@ -8,6 +10,8 @@ class BitStream
     fstream fileStream;
     char byteBuffer = 0x00;
     int bitCounter = 0;
+    int byteCounter = 0;
+    int fileByteSize=0;
     
     public:
     BitStream(const string &file, char op)
@@ -15,13 +19,16 @@ class BitStream
         if (op == 'r')
         {
             this->fileStream.open(file,ios::in);
+            this->fileStream.ignore( std::numeric_limits<std::streamsize>::max() );
+            fileByteSize = this->fileStream.gcount();
+            this->fileStream.clear();   //  Since ignore will have set eof.
+            this->fileStream.seekg( 0, std::ios_base::beg );
         }
         else if (op == 'w')
         {
             this->fileStream.open(file,ios::out);
         }
-        else
-        {
+        else{
             cout << " MODO DE UTILIZAÇÂO: BitStream('filename','r/w')" << endl;
         }
 
@@ -31,6 +38,8 @@ class BitStream
         unsigned char readBit();
         void writeNBits(unsigned char buffer);
         unsigned char readNBits();
+        void setByteOnBuffer();
+        void close();
 };
 
 void BitStream::writeBit(unsigned char bit)
@@ -38,19 +47,36 @@ void BitStream::writeBit(unsigned char bit)
     if (bit == '0') 
     {
         this->byteBuffer = this->byteBuffer << 1;
-        bitCounter++;
+        this->bitCounter++;
     }
     else 
     {
-        this->byteBuffer = this->byteBuffer << 1;
-        this->byteBuffer = this->byteBuffer | 0x1;
-        bitCounter++;
+        this->byteBuffer = (this->byteBuffer << 1 ) | 0x1;
+        this->bitCounter++;
     }
-    if(bitCounter == 8) this->fileStream << byteBuffer << endl;
+    if(bitCounter == 8){
+        this->fileStream << byteBuffer;
+        this->bitCounter=0;
+    } 
 }
 
 unsigned char BitStream::readBit()
 {
+    setByteOnBuffer();
+    char out = (this->byteBuffer >> this->bitCounter) & 0x01;
+    this->bitCounter++;
+    if(this->bitCounter==8)
+    {
+        this->bitCounter = 0; 
+        this->byteCounter++;
+    }
+    if (out==0x00) 
+    {
+        return '0';
+    }else 
+    {
+        return '1';
+    }
 }
 
 void BitStream::writeNBits(unsigned char buffer)
@@ -59,4 +85,16 @@ void BitStream::writeNBits(unsigned char buffer)
 
 unsigned char BitStream::readNBits()
 {    
+}
+
+void BitStream::setByteOnBuffer()
+{
+    this->byteBuffer = fileStream.get();
+    fileStream.seekg(this->byteCounter);
+}
+
+void BitStream::close()
+{
+    cout << "Closing File" << endl;
+    this->fileStream.close();
 }
--- PartA/Ex1/example.cpp
+++ PartA/Ex1/example.cpp
@@ -11,4 +11,52 @@ int main()
     bs.writeBit('0');
     bs.writeBit('1');
     bs.writeBit('1');
+
+    bs.writeBit('0');
+    bs.writeBit('1');
+    bs.writeBit('0');
+    bs.writeBit('0');
+    bs.writeBit('0');
+    bs.writeBit('0');
+    bs.writeBit('0');
+    bs.writeBit('1');
+    bs.close();
+
+    BitStream bs2("teste.bits",'r');
+
+    
+    char out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+
+    cout << "New word " << endl;
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    out = bs2.readBit();
+    cout  << out << endl;
+    bs2.close();
 }
