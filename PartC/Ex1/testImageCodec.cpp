b735a391d14a1f0b646a0a7f6246d838d51d61fb
tiagoadonis <tiago.adonis@ua.pt>
2021-12-22 00:34:00 +0000

    Added Predictive Coding

--- PartC/Ex1/testImageCodec.cpp
+++ PartC/Ex1/testImageCodec.cpp
@@ -6,5 +6,9 @@ using namespace std;
 
 int main(){
     ImageCodec codec = ImageCodec("monarch.ppm");
-    codec.convertToYUV420("newImage.ppm");
+    codec.convertToYUV420("monarchYUV420.ppm");
+
+    CImg<unsigned char> img("monarchYUV420.ppm");
+    CImg<unsigned char> imgPredicted = codec.losslessJPEG(img);
+    imgPredicted.save("predictedImage.ppm");
 }
